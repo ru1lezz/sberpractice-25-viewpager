baf7e86fd785ea04fc250a3b01d51975cee8f7d2
serzhan <serzhan@tsconsulting.ru>
2019-03-13 15:40:56 +0300

    add ScaleGestureDetector to zoom image

--- app/src/main/java/com/example/android/viewpager/ImageFragment.java
+++ app/src/main/java/com/example/android/viewpager/ImageFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.viewpager;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -16,6 +19,8 @@ public class ImageFragment extends Fragment {
     private static final String EXTRA_URI = "extra_uri";
 
     private ImageView mImageView;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
 
     public ImageFragment() {
     }
@@ -27,6 +32,7 @@ public class ImageFragment extends Fragment {
         return view;
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -34,7 +40,13 @@ public class ImageFragment extends Fragment {
         mImageView.setImageURI(getArguments().getParcelable(EXTRA_URI));
         mImageView.setMaxWidth(300);
         mImageView.setMaxHeight(300);
+        mScaleGestureDetector = new ScaleGestureDetector(view.getContext(), new ScaleListener());
+        view.setOnTouchListener((v, event) -> {
+            mScaleGestureDetector.onTouchEvent(event);
+            return true;
+        });
     }
+
 
     public static ImageFragment newInstance(Uri uri) {
         Bundle args = new Bundle();
@@ -42,5 +54,17 @@ public class ImageFragment extends Fragment {
         ImageFragment fragment = new ImageFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f,
+                    Math.min(mScaleFactor, 10.0f));
+            mImageView.setScaleX(mScaleFactor);
+            mImageView.setScaleY(mScaleFactor);
+            return true;
+        }
     }
 }
